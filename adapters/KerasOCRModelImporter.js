9db406c946f4977ee479b9fada53d39738c159d8
Marco Steinke <49523050+MarcoSteinke@users.noreply.github.com>
2022-07-12 17:19:27 +0200

    apply changes

--- adapters/KerasOCRModelImporter.js
+++
@@ -1,9 +0,0 @@
-import * as tf from '@tensorflow/tfjs';
-
-const importModel = (resource) => { return tf.loadLayersModel(resource) };
-
-module.exports = {
-    importModel: importModel
-};
-
-const model = importModel("exports/keras_ocr/model.json");
